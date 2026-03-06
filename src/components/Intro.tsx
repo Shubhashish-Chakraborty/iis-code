2ab2207288356b1da94016f784b1aa2f09eb2809
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-03-06 22:18:50 +0530

    wording

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,7 @@
 export const Intro = () => {
     return (
         <div className="text-center">
-            <h1 className="text-white text-3xl md:text-6xl lg:text-6xl font-bold tracking-tight max-w-4xl mx-auto leading-tight">Practical Exam Codes? You know {" "} 
+            <h1 className="text-white text-3xl md:text-6xl lg:text-6xl font-bold tracking-tight max-w-4xl mx-auto leading-tight">Practical Exam Codes? Guess {" "} 
                 <span onClick={() => {
                     window.open("https://shubhashish.me")
                 }} className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent decoration-TextCap2 cursor-pointer hover:underline">who's Back</span> 💀
